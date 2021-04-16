eaae86bd71278d9b6880bd3767d8a4e53785bfb2
Nitweeg <maxime.tredez@hotmail.fr>
2021-04-16 15:52:22 +0100

    NavBar

--- DiffusionStage/src/app/app.module.ts
+++ DiffusionStage/src/app/app.module.ts
@@ -3,9 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import {MatGridListModule} from '@angular/material/grid-list';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderAdminComponent } from './header-admin/header-admin.component';
 import { ToolBarreComponent } from './tool-barre/tool-barre.component';
+import { MatToolbarModule } from  '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -17,7 +24,14 @@ import { ToolBarreComponent } from './tool-barre/tool-barre.component';
     MatGridListModule,
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
