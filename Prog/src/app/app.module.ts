7d6f5e9c9f25d67d6caa026effb321e1cef36cad
Nitweeg <maxime.tredez@hotmail.fr>
2021-04-16 08:50:52 +0100

    Tuto

--- Prog/src/app/app.module.ts
+++ Prog/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {NgForm} from '@angular/forms';
+import {AccueilComponent} from './accueil/accueil.component'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MonPremierComponent } from './mon-premier/mon-premier.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MonPremierComponent
+    AccueilComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
