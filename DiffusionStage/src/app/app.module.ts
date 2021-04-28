f3e26a9196524f50d436e05505f7109bd077360a
Nitweeg <maxime.tredez@hotmail.fr>
2021-04-28 06:40:00 +0100

    ToolbarreV2

--- DiffusionStage/src/app/app.module.ts
+++ DiffusionStage/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatButtonModule} from '@angular/material/button';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { ScansComponent } from './scans/scans.component';
 import {MatExpansionModule} from '@angular/material/expansion';
+import {MatDividerModule} from '@angular/material/divider';
 
 
 @NgModule({
@@ -35,6 +36,7 @@ import {MatExpansionModule} from '@angular/material/expansion';
     FlexLayoutModule,
     FormsModule,
     MatExpansionModule,
+    MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
