e0dd6b13df1736eac369008700116de1e03e6050
Nitweeg <maxime.tredez@hotmail.fr>
2021-04-28 13:26:27 +0100

    Dif

--- DiffusionStage/src/app/diffusion/diffusion.component.ts
+++ DiffusionStage/src/app/diffusion/diffusion.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-diffusion',
   templateUrl: './diffusion.component.html',
   styleUrls: ['./diffusion.component.css']
 })
-export class DiffusionComponent implements OnInit {
 
+export class DiffusionComponent implements OnInit {
+  
   constructor() { }
 
   ngOnInit(): void {
